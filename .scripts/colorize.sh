514f150c578e971ef9561c9454ccd0b64c21abf4
Lukas Levickas <l.levickas@protonmail.com>
2016-12-12 11:21:21 +0200

    meh

--- .scripts/colorize.sh
+++ .scripts/colorize.sh
@@ -9,6 +9,14 @@ output_8="/tmp/colors-8-output"
 trap "rm -f $tmpA1 $tmpB1; exit 0" 0   #remove temp files
 trap "rm -f $tmpA1 $tmpB1; exit 1" 1 2 3 15  #remove temp files
 
+print_color_palette(){
+	for i in {0..15}
+	do
+		echo " $i)	$(tput setaf $i) ███"
+		printf '\e[0m'
+	done
+}
+
 generate(){
 	if [ ! -f "$1" ]
 	then
@@ -35,6 +43,7 @@ randomize(){
 }
 
 recolor_xres(){
+	specific_color="$1"
 	echo "recoloring Xresources..."
 	colors=($(<$output_16))
 	config="$HOME/.Xresources"
@@ -44,13 +53,47 @@ recolor_xres(){
 	then
 		cp $config $config_backup
 	fi
-	for i in ${!colors[@]}
-	do
-		line="$(printf '#define base0%x' $i)"
-		xres=$(echo "$xres" | sed -E "s/$line #.{6}/$line #${colors[i]}/ig")
-	done
+	if [[ -z $specific_color ]]
+	then
+		for i in ${!colors[@]}
+		do
+			line="$(printf '#define base0%x' $i)"
+			xres=$(echo "$xres" | sed -E "s/$line #.{6}/$line #${colors[i]}/ig")
+		done
+	else
+		line="$(printf '#define base0%x' $specific_color)"
+		current_color=$(echo "$xres" | awk "/$line/ {print \$3}" | tr -d '# \n')
+		n=$(echo ${colors[@]} | tr -s " " "\n" | grep -n "$current_color" | cut -d":" -f 1)
+		if [[ $n -ge ${#colors[@]} ]]
+		then
+			n=0
+		fi
+		xres=$(echo "$xres" | sed -E "s/$line #.{6}/$line #${colors[n]}/ig")
+	fi
 	echo "$xres" > $config
 	xrdb $config
+	killall -USR1 urxvt
+}
+
+switch_xres(){
+	config="$HOME/.Xresources"
+	config_backup="$config-backup"
+	xres=$(cat $config)
+	if [ ! -f $config_backup ]
+	then
+		cp $config $config_backup
+	fi
+	colors=($(echo "$xres" | awk '/#define base0. #.{0,8}/ {print $3}' | cut -c 2-7))
+	echo ${colors[0]}
+	color1=$1
+	color2=$2
+	line1="$(printf '#define base0%x' $color1)"
+	line2="$(printf '#define base0%x' $color2)"
+	xres=$(echo "$xres" | sed -E "s/$line1 #.{6}/$line1 #${colors[$color2]}/ig")
+	xres=$(echo "$xres" | sed -E "s/$line2 #.{6}/$line2 #${colors[$color1]}/ig")
+	echo "$xres" > $config
+	xrdb $config
+	killall -USR1 urxvt
 }
 
 recolor_bspwm(){
@@ -114,6 +157,21 @@ recolor_conky(){
 	done
 }
 
+cycle(){
+while true
+do
+	if [[ -z $5 ]]
+	then
+		clear
+		"$1" -r "$2"
+		$3
+		sleep "$4"
+	else
+		"$1" -r "$2" && urxvt -e sh -c "$3" && sleep "$4"
+	fi
+done
+}
+
 
 case "$1" in
 	--generate | -g)
@@ -123,9 +181,15 @@ case "$1" in
 	--randomize | -r)
 		if [[ $2 =~ "x" ]]
 		then
-			echo "randomizing Xresources..."
-			randomize $output_16
-			recolor_xres
+			if [[ -z $3 ]]
+			then
+				echo "randomizing Xresources..."
+				randomize $output_16
+				recolor_xres
+			else
+				echo "randomizing Xresources..."
+				recolor_xres $3
+			fi
 		elif [[ $2 =~ "bspwm" ]]
 		then
 			randomize $output_8
@@ -146,6 +210,22 @@ case "$1" in
 	--bspwm | -b)
 		recolor_bspwm
 		;;
+	--cycle | -c)
+		echo "kill the loop with Ctrl-C when happy"
+		if [[ $2 =~ "x" ]]
+		then
+			cycle $0 "x" "killall -USR1 urxvt" 1
+		elif [[ $2 =~ "bspwm" ]]
+		then
+			cycle $0 "bspwm" "" 1
+		elif [[ $2 =~ "polybar" ]]
+		then
+			cycle $0 "polybar" "" 2
+		elif [[ $2 =~ "conky" ]]
+		then
+			cycle $0 "conky" "" 2
+		fi
+		;;
 	--everything | -e)
 		generate $2 "16" $output_16
 		generate $2 "8" $output_8
@@ -154,5 +234,11 @@ case "$1" in
 		recolor_polybar
 		recolor_conky
 		feh --bg-fill $2
+		;;
+	--print-palette | -p)
+		print_color_palette
+		;;
+	--switch | -s)
+		switch_xres $2 $3
 		;;
 esac
