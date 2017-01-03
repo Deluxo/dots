3bfad4bd4bd098dc247043fd39b5158b948e65b1
Lukas Levickas <l.levickas@protonmail.com>
2017-01-03 20:21:40 +0200

    update mpv config

--- .scripts/context-runner.sh
+++ .scripts/context-runner.sh
@@ -7,14 +7,14 @@ context=$(xclip -o);
 if [[ $context =~ "twitch.tv" ]]; then
 	livestreamer $context best --player mpv&!
 elif [[ $context =~ "youtube.com" || $context =~ "youtu.be" ]]; then
-	mpv $context
+	mpv --fs $context
 # Torrent
 elif [[ $context =~ "magnet:" ]]; then
 	peerflix "$context" -f "Downloads/" -k &!
 elif [[ $context =~ ".torrent" ]]; then
 	peerflix "$context" -f "Downloads/" -k &!
 elif [[ $context =~ "http" ]]; then
-	mpv $context
+	mpv --fs $context
 else
 	notify-send "Clipboard" "No possible solutions to clipboard data found";
 fi
