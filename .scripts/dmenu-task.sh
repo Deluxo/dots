648362c1a7d9c02c320719ae602f0522687d7914
Lukas Levickas <l.levickas@protonmail.com>
2017-01-25 16:17:57 +0200

    up

--- .scripts/dmenu-task.sh
+++ .scripts/dmenu-task.sh
@@ -1,18 +1,25 @@
 #! /bin/sh
 
+main() {
+	task ls | head -n -2 | tail -n +4 | sort
+}
+
+add() {
+	task add $(echo $1 | tail -c +2) >> /dev/null
+}
+
+finish() {
+	task done "$(echo $@ | awk '{print $2;}')" >> /dev/null
+}
+
 if [ -z "$1" ]
 then
-	task ls | head -n -2 | tail -n +4 | sort
+	main
 else
 	if [[ "$1" =~ ^\+  ]]; then
-		if [[ "$1" =~ due: ]]; then
-			due=$(echo "$1" | grep -o 'due:.*' | sed 's/due://gi')
-			dsc=$(echo "$1" | sed "s/due:$due//gi")
-			task add "$(echo $dsc | tail -c +2)" due:$due
-		else
-			task add "$(echo $1 | tail -c +2)"
-		fi
+			add "$1"
 	elif [[ "$1" =~ ^-.[0-9] ]]; then
-		task done "$(echo $@ | awk '{print $2;}')"
+		finish $@
 	fi
+	main
 fi
