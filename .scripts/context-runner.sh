9dcbe4e0d52882c1788e2ef1722d4b6855cb2169
Lukas Levickas <l.levickas@protonmail.com>
2017-02-21 00:56:50 +0200

    up

--- .scripts/context-runner.sh
+++ .scripts/context-runner.sh
@@ -1,20 +1,32 @@
 #! /bin/zsh
 
 context=$(xclip -o);
+msg="couldn't find anything to play"
 
-
-# Video
 if [[ $context =~ "twitch.tv" ]]; then
+	msg="Twitch.tv"
 	livestreamer $context best --player mpv&!
-elif [[ $context =~ "youtube.com" || $context =~ "youtu.be" ]]; then
-	mpv $context
-# Torrent
+elif [[ $context =~ "youtube" ]]; then
+	msg="Youtube"
+	mpv $context &!
 elif [[ $context =~ "magnet:" ]]; then
+	msg="Magnet"
 	peerflix "$context" -f "Downloads/" -k &!
 elif [[ $context =~ ".torrent" ]]; then
+	msg="selected torrent file"
 	peerflix "$context" -f "Downloads/" -k &!
 elif [[ $context =~ "http" ]]; then
-	mpv $context
-else
-	notify-send "Clipboard" "No possible solutions to clipboard data found";
+	msg="Some http stream"
+	mpv $context &!
+elif ls ~/Downloads/*.torrent 1> /dev/null 2>&1; then
+	file=$(ls -1t ~/Downloads/*.torrent | head -n 1)
+	fileModDate=$(stat -c %Y "$file")
+	now=$(date +%s)
+	freshness=$(($now-$fileModDate))
+	if [[ $freshness -lt 3000 ]]; then
+		msg="Latest torrent file"
+		peerflix "$file" -f "Downloads/" -k &!
+	fi
 fi
+
+notify-send "Context Runner" "$msg"
