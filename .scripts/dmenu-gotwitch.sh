09baf948086aa395dcee846814e72448690952a4
Lukas Levickas <l.levickas@protonmail.com>
2017-02-25 17:40:16 +0200

    nice update
    gotwitch dmenu script uses temp files instead fetching new list
    every time rofi opens

--- .scripts/dmenu-gotwitch.sh
+++ .scripts/dmenu-gotwitch.sh
@@ -1,16 +1,43 @@
 #! /bin/sh
 
+tmpDir="/tmp"
+filePrefix="gotwitch-"
+subscriptionsFile="$tmpDir"/"$filePrefix"subscriptions
+popularFile="$tmpDir"/"$filePrefix"popular
+updateFreqInSec="60"
+
+if [[ ! -e "$popularFile" ]]; then
+	gotwitch streams -a > $popularFile;
+fi
+if [[ ! -e "$subscriptionsFile" ]]; then
+	gotwitch streams -ba > $subscriptionsFile;
+fi
+
+
+fileModDate=$(stat -c %Y "$popularFile")
+now=$(date +%s)
+freshness=$(($now-$fileModDate))
+
+if [[ $freshness -gt $updateFreqInSec ]]; then
+	gotwitch streams -a > $popularFile;
+fi
+
+if [[ $freshness -gt $updateFreqInSec ]]; then
+	gotwitch streams -ba > $subscriptionsFile;
+fi
+
+
 if [ -z "$@" ] || [ "$@" == "Back" ]
 then
-	echo Subscribtions;
+	echo Subscriptions;
 	echo Popular
 else
-	if [ "$@" == "Subscribtions" ]; then
+	if [ "$@" == "Subscriptions" ]; then
 		echo Back;
-		gotwitch streams -ba
+		cat "$subscriptionsFile";
 	elif [ "$@" == "Popular" ]; then
 		echo Back;
-		gotwitch streams -a
+		cat "$popularFile";
 	else
 		channel=$(echo $@ | awk '{print $1;}')
 		gotwitch -s $channel
