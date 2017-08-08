de935cbe3fd629406b059a40e99f4fc2622e59ab
Lukas Levickas <l.levickas@protonmail.com>
2017-08-08 23:04:58 +0300

    blueberries, updated gotwitch rofi script

--- .scripts/dmenu-gotwitch-from-file.sh
+++ .scripts/dmenu-gotwitch-from-file.sh
@@ -1,10 +1,38 @@
 #! /bin/sh
 
-if [[ -z "$@" ]]; then
+sep="- - - - - - - - - -"
+
+streamers()
+{
+	echo "games"
+	echo $sep
 	cat /tmp/gotwitch-subscriptions
-	echo - - - - - - - - - -
+	echo $sep
 	cat /tmp/gotwitch-popular
+}
+
+game()
+{
+	gotwitch game -l 1000 -o $1 | sed 's/^/games: /gi'
+	echo "games: offset $(($1 + 1))"
+}
+
+watch()
+{
+	gotwitch -w $1
+}
+
+if [[ -z "$@" ]]; then
+	streamers
+elif [[ "$@" == "games" ]]; then
+	game 0
+elif [[ "$@" =~ "games: offset " ]]; then
+	offset=$(echo $1 | awk '{print $3;}')
+	game $offset
+elif [[ "$@" =~ "games: " ]]; then
+	game=$(echo $@ | sed 's/games: //gi')
+	gotwitch -q "$game"
 else
-	channel=$(echo $@ | awk '{print $1;}')
-	gotwitch -w $channel
+	channel=$(echo $1 | awk '{print $1;}')
+	watch "$channel"
 fi
