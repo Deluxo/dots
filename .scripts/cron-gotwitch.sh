b951a5ab987023d524f5d3e89b82e438f391c8ec
Lukas Levickas <l.levickas@protonmail.com>
2018-03-15 22:50:12 +0200

    make cron-gotwitch.sh also show streamers' logos

--- .scripts/cron-gotwitch.sh
+++ .scripts/cron-gotwitch.sh
@@ -2,13 +2,15 @@
 
 export DISPLAY=:0.0
 
-popsFile=/tmp/gotwitch-popular
-subsFile=/tmp/gotwitch-subscriptions
-notifiedFile=/tmp/gotwitch-notified
-touch $notifiedFile
+tmpDir=/tmp/gotwitch
+popsFile=$tmpDir/gotwitch-popular
+subsFile=$tmpDir/gotwitch-subscriptions
+notifiedFile=$tmpDir/gotwitch-notified
 
-gotwitch -lbg --padding 0 > $subsFile
-gotwitch -lg --padding 0 > $popsFile
+mkdir -p $tmpDir
+touch $notifiedFile
+gotwitch -lbgt --padding 0 > $subsFile
+gotwitch -lgt --padding 0 > $popsFile
 
 subs=$(cat $subsFile)
 notified=$(cat $notifiedFile)
@@ -19,14 +21,22 @@ for n in $notify; do
 
 	oldStream=$(echo "$notified" | grep "$n" | grep -v "$inSubsFile")
 	if [[ -n "$oldStream" ]]; then
+		echo "lol"
 		notified=$(echo "$notified" | grep -vi $oldStream)
 	fi;
 
 	inNotifiedFile=$(echo "$notified" | grep "$inSubsFile")
 	if [[ ! -n "$inNotifiedFile" ]]; then
-		entry=$(echo "$inSubsFile" | sed "s/ / is playing /1")
-		notify-send -t 10000 "Gotwitch" "$entry"
+		entry=$(echo "$inSubsFile" | sed "s/ / is playing /1" | sed "s/ (http.*$//")
+		icon=$(echo "$inSubsFile" | grep -oe "http.*[^)]")
+		localIcon=$(echo $icon | sed "s/http.*\///")
+		localIcon=$tmpDir/$localIcon
+		if [[ ! -f $localIcon ]]; then
+			curl -s "$icon" > $localIcon
+		fi
+		notify-send -t 15000 -i "$localIcon" "${(C)n}" "$entry"
 		notified=$(echo "$notified\n$inSubsFile")
 	fi
 done
-echo $notified > /tmp/gotwitch-notified
+
+echo $notified > $notifiedFile
