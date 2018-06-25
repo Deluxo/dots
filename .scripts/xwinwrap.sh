d982230c1aa3e54a69c198dfc732ad95cc2dc7e2
Lukas Levickas <l.levickas@protonmail.com>
2018-06-26 00:44:11 +0300

    up

--- .scripts/xwinwrap.sh
+++ .scripts/xwinwrap.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-xwinwrap -ov -ni -fs -- mpv --no-stop-screensaver -wid WID --keepaspect=yes --volume 0 --panscan 1 --loop "$1" &!
+killall xwinwrap
+
+nice -n 19 xwinwrap -ov -ni -fs -- mpv --no-stop-screensaver -wid WID --keepaspect=yes --volume 0 --panscan 1 --loop "$1" --speed 1.5 -ao null --af='' > /dev/null 2>&1 &!
