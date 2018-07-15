6a19d31dc71413fd8f98289db473103a0f170e85
Lukas Levickas <l.levickas@protonmail.com>
2018-07-15 11:02:19 +0300

    up wallpaper

--- .scripts/xwinwrap.sh
+++ .scripts/xwinwrap.sh
@@ -2,4 +2,4 @@
 
 killall xwinwrap
 
-nice -n 19 xwinwrap -ov -ni -fs -- mpv --no-stop-screensaver -wid WID --keepaspect=yes --volume 0 --panscan 1 --loop "$1" --speed 1.2 -ao null --af='' > /dev/null 2>&1 &!
+nice -n 19 xwinwrap -ov -ni -fs -- mpv --no-stop-screensaver -wid WID --keepaspect=yes --volume 0 --panscan 1 --loop "$1" --speed 1 -ao null --af='' > /dev/null 2>&1 &!
