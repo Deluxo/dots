218f11fd9db346217a7f6ee6bef896a8bca92d58
Lukas Levickas <l.levickas@protonmail.com>
2018-10-25 17:55:39 +0300

    refresh

--- .config/polybar/scripts/mpris.sh
+++ .config/polybar/scripts/mpris.sh
@@ -4,13 +4,14 @@ icon=""
 player_status=$(playerctl status 2> /dev/null)
 
 if [[ $? -eq 0 ]]; then
-    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+	#metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+	metadata=""
 fi
 
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#FFFFFFFF}$icon   $metadata"
+    echo "%{F#FFFFFFFF}$icon$metadata"
 elif [[ $player_status = "Paused" ]]; then
-    echo "%{F#AAFFFFFF}$icon   $metadata"
+    echo "%{F#AAFFFFFF}$icon$metadata"
 else
     echo "%{F#AAFFFFFF}$icon"
 fi
