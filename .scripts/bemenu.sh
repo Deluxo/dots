a9710e8ae65356ded4ca7fab3aa7ed0205624c9e
Lukas Levickas <l.levickas@protonmail.com>
2020-08-09 09:52:57 +0300

    chore(dots): colors base16 horizon dark

--- .scripts/bemenu.sh
+++ .scripts/bemenu.sh
@@ -1,37 +1,27 @@
-#!/bin/bash
+#! /bin/sh
 
-#bemenu \
-	#-b \
-	#--fn "Hack 15"
-	#--tb                  defines the title background color. (wx)
-	#--tf                  defines the title foreground color. (wx)
-	#--fb                  defines the filter background color. (wx)
-	#--ff                  defines the filter foreground color. (wx)
-	#--nb                  defines the normal background color. (wx)
-	#--nf                  defines the normal foreground color. (wx)
-	#--hb                  defines the highlighted background color. (wx)
-	#--hf                  defines the highlighted foreground color. (wx)
-	#--sb                  defines the selected background color. (wx)
-	#--sf                  defines the selected foreground color. (wx)
-	#--scb                 defines the scrollbar background color. (wx)
-	#--scf                 defines the scrollbar foreground color. (wx)
+color0='#1c1e26'
+color1='#e93c58'
+color2='#efaf8e'
+color3='#efb993'
+color4='#df5273'
+color5='#b072d1'
+color6='#24a8b4'
+color7='#cbced0'
 
+export BEMENU_BACKEND=wayland
 
-# vim: set ft=sh sw=4 et:
-
-MODE="$1"
-
-if [[ $MODE == "commands" ]]; then
-    COMMAND="compgen -c | sort -u | fzf --no-extended --print-query --bind ctrl-n:replace-query | tail -n1"
-elif [[ $MODE == "pass" ]]; then
-    COMMAND="find $HOME/.config/password-store -type f -name '*.gpg' -printf '%P\\n' | awk '{ print substr(\$0, 0, length(\$0)-4) }' | fzf --bind ctrl-n:replace-query"
-else
-    exit 1
-fi
-
-TMP=$(mktemp)
-
-alacritty --class "alacritty-menu" -e bash -c "$COMMAND > $TMP"
-
-cat "$TMP"
-rm "$TMP"
+bemenu-run \
+-b \
+-i \
+-w \
+-p '❯' \
+--fn 'Overpass Semibold 18px' \
+--nb "$color0" \
+--tb "$color0" \
+--fb "$color0" \
+--ff "$color2" \
+--hb "$color0" \
+--sb "$color0" \
+--scb "$color0" \
+--nf "$color7" \
